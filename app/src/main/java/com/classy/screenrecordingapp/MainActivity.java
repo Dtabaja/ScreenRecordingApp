e21f411db9807b30893582c0346a28a80d6295d8
Daniel Tabaja <daniel.tabaja@s.afeka.ac.il>
2021-02-25 14:38:45 +0200

    Initial commit

---
+++ app/src/main/java/com/classy/screenrecordingapp/MainActivity.java
@@ -0,0 +1,318 @@
+package com.classy.screenrecordingapp;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.MediaRecorder;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+import android.widget.VideoView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final int REQUEST_CODE = 1000;
+    private static final int REQUEST_PERMISSION = 1001;
+
+    private MediaProjectionManager mediaProjectionManager;
+    private MediaProjection mediaProjection;
+    private VirtualDisplay virtualDisplay;
+    private MediaProjectionCallback mediaProjectionCallback;
+    private MediaRecorder mediaRecorder;
+
+    private int mScreenDensity;
+    private static int DISPLAY_WIDTH = 720;
+    private static int DISPLAY_HEIGHT = 1280;
+
+
+    //View
+    private RelativeLayout rootLayout;
+    private ToggleButton toggleButton;
+    private VideoView videoView;
+    private String videoUri = "";
+    private String fileURI = "C:\\Users\\Daniel Tabaja\\Desktop";
+    private StorageReference storageReference;
+    private DatabaseReference databaseReference;
+    UploadTask uploadTask;
+
+    private static void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+        Task<Uri> downloadUrl = taskSnapshot.getMetadata().getReference().getDownloadUrl();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        mScreenDensity = metrics.densityDpi;
+        findViews();
+        mediaRecorder = new MediaRecorder();
+        mediaProjectionManager = (MediaProjectionManager) getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        //Get Screen
+        DISPLAY_HEIGHT = metrics.heightPixels;
+        DISPLAY_WIDTH = metrics.widthPixels;
+
+
+        storageReference = FirebaseStorage.getInstance().getReference();
+        // databaseReference = FirebaseDatabase.getInstance().getReference("video");
+
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        + ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECORD_AUDIO)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                            ||
+                            ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.RECORD_AUDIO)) {
+                        toggleButton.setChecked(false);
+                        Snackbar.make(rootLayout, "permissions", Snackbar.LENGTH_INDEFINITE)
+                                .setAction("ENABLE", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+
+                                        ActivityCompat.requestPermissions(MainActivity.this,
+                                                new String[]{
+
+                                                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                                        Manifest.permission.RECORD_AUDIO
+
+                                                }, REQUEST_PERMISSION);
+                                    }
+                                }).show();
+                    } else {
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{
+
+                                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                        Manifest.permission.RECORD_AUDIO
+
+                                }, REQUEST_PERMISSION);
+                    }
+                } else {
+                    toogleScreenShare(v);
+                }
+            }
+
+        });
+    }
+
+    public void findViews() {
+        videoView = findViewById(R.id.videoView);
+        toggleButton = findViewById(R.id.toggleButton);
+        rootLayout = findViewById(R.id.rootLayout);
+    }
+
+    private void toogleScreenShare(View v) {
+
+        if (((ToggleButton) v).isChecked()) {
+            initRecorder();
+            recorderScreen();
+        } else {
+            mediaRecorder.stop();
+            mediaRecorder.reset();
+            UploadVideo();
+            stopRecordScreen();
+
+            //Play in Video View
+            videoView.setVisibility(View.VISIBLE);
+            videoView.setVideoURI(Uri.parse(videoUri));
+            videoView.start();
+
+        }
+    }
+
+    private void recorderScreen() {
+
+        if (mediaProjection == null) {
+            startActivityForResult(mediaProjectionManager.createScreenCaptureIntent(), REQUEST_CODE);
+            return;
+        }
+        virtualDisplay = createVirtualDisplay();
+        mediaRecorder.start();
+    }
+
+    private VirtualDisplay createVirtualDisplay() {
+
+        return mediaProjection.createVirtualDisplay("MainActivity",
+                DISPLAY_WIDTH, DISPLAY_HEIGHT, mScreenDensity,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                mediaRecorder.getSurface(), null, null);
+    }
+
+    private void initRecorder() {
+
+        try {
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+
+            videoUri = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES)
+                    + new StringBuilder("/ScreenRecordingApp").append(new SimpleDateFormat("dd-MM-yyyy-hh_mm_ss")
+                    .format(new Date())).append(".mp4").toString();
+
+            mediaRecorder.setOutputFile(videoUri);
+            mediaRecorder.setVideoSize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+            mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.setVideoEncodingBitRate(512 * 1000);
+            mediaRecorder.setVideoFrameRate(100);
+            mediaRecorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode != REQUEST_CODE) {
+            Toast.makeText(this, "Unk Error", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (resultCode != RESULT_OK) {
+            Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mediaProjectionCallback = new MediaProjectionCallback();
+        mediaProjection = mediaProjectionManager.getMediaProjection(resultCode, data);
+        mediaProjection.registerCallback(mediaProjectionCallback, null);
+        virtualDisplay = createVirtualDisplay();
+        mediaRecorder.start();
+    }
+
+    private class MediaProjectionCallback extends MediaProjection.Callback {
+
+        @Override
+        public void onStop() {
+            if (toggleButton.isChecked()) {
+                toggleButton.setChecked(false);
+                mediaRecorder.stop();
+                mediaRecorder.reset();
+            }
+            mediaProjection = null;
+            stopRecordScreen();
+            super.onStop();
+        }
+    }
+
+    private void stopRecordScreen() {
+        if (virtualDisplay == null)
+            return;
+        virtualDisplay.release();
+        destroyMediaProjection();
+
+    }
+
+    private void destroyMediaProjection() {
+        if (mediaProjection != null) {
+            mediaProjection.unregisterCallback(mediaProjectionCallback);
+            mediaProjection.stop();
+            mediaProjection = null;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_PERMISSION:
+                if ((grantResults.length > 0) && (grantResults[0] + grantResults[1] == PackageManager.PERMISSION_GRANTED)) {
+                    toogleScreenShare(toggleButton);
+                } else {
+                    toggleButton.setChecked(false);
+                    Snackbar.make(rootLayout, "permissions", Snackbar.LENGTH_INDEFINITE)
+                            .setAction("ENABLE", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+
+                                    ActivityCompat.requestPermissions(MainActivity.this,
+                                            new String[]{
+
+                                                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                                    Manifest.permission.RECORD_AUDIO
+
+                                            }, REQUEST_PERMISSION);
+                                }
+                            }).show();
+                }
+                return;
+        }
+    }
+
+    private String getExt(Uri uri) {
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+
+    private void UploadVideo() {
+        Log.d("ptttttt", "im here: ");
+        StorageReference filePath = storageReference.child((new SimpleDateFormat("dd-MM-yyyy").
+                format(new Date()))).child(new SimpleDateFormat("hh_mm_ss")
+                .format(new Date())+".mp4");
+        Log.d("ptttttt", "im here2: ");
+        Uri uri = Uri.fromFile(new File(videoUri));
+        Log.d("ptttttt", "im here3: ");
+        filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Log.d("ptttttt", "im here4: ");
+                Toast.makeText(MainActivity.this, "Data saved To Movies Directory", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+    }
+}
+
+
+
+
+
+
+
+
